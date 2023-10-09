b297b958de4a75a9088f6471e9dae31e6a7011b4
TopiPajukko <topi.pajukko@dnainternet.net>
2023-10-09 14:49:59 +0300

    part1 teht

---
+++ part1/src/App.jsx
@@ -0,0 +1,46 @@
+/* eslint-disable react/prop-types */
+const Header = (props) => {
+  return (
+  <div>
+  <h1>{props.course}</h1>
+</div>
+  )
+}
+
+const Content = (props) => {
+  return (
+  <div>
+  <p>{props.partName} {props.exercises}</p>
+  </div>
+  )
+}
+
+const Total = (props) => {
+  return (
+  <div>
+  <p>Number of exercises {props.exercises1 + props.exercises2 + props.exercises3}</p>
+  </div>
+  )
+}
+
+const App = () => {
+  const courseData = 'Half Stack application development'
+  const part1 = 'Fundamentals of React'
+  const exercises1 = 10
+  const part2 = 'Using props to pass data'
+  const exercises2 = 7
+  const part3 = 'State of a component'
+  const exercises3 = 14
+
+  return (
+    <div>
+      <Header course = {courseData}/>
+      <Content partName = {part1} exercises = {exercises1}/>
+      <Content partName = {part2} exercises = {exercises2}/>
+      <Content partName = {part3} exercises = {exercises3}/>
+      <Total exercises1 = {exercises1} exercises2 = {exercises2} exercises3 = {exercises3} />
+    </div>
+  )
+}
+
+export default App
